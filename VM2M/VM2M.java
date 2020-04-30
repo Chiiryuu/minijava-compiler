f02c0c81a59c6fc51afd6d616f7140caac38f09d
Brandon Percin <Chiiryuusuki@gmail.com>
2020-04-30 00:06:14 -0600

    Works on Factorial, MT4, Tree Visitor, Binary Tree

---
+++ VM2M/VM2M.java
@@ -0,0 +1,51 @@
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+
+public class VM2M {
+
+  public static void V2VM() throws IOException {
+
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<String> inputLines = new ArrayList<>();
+    String inputRaw = "";
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      line = line.replaceAll("\\s"," ");
+      inputRaw+=line+'\n';
+      inputLines.add(line);
+    }
+    scanner.close();
+
+    CodeConverter table = new CodeConverter();
+    table.debug = true;
+    table.vaporMProgram=inputLines;
+    table.convertCode();
+
+    //System.out.println("# Vars: "+table.vars.size());
+    //table.printVarArr();
+
+    table.printMIPS();
+
+    //System.out.println("Raw Program:");
+    //System.out.println(programString);
+  }
+
+  //$a0 - $a3: input/output arguments
+  //in[] : overflow input arguments
+  //out[] : overflow output arguments
+  //$v0: return value register
+
+  public static void main(String[] args) throws IOException {
+
+
+    V2VM();
+
+  }
+}
+
+
